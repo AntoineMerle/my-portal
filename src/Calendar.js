94182fea2c6a2baf9d2ca64db97ef39facdee77b
AntoineMerle <am92300@gmail.com>
2016-12-12 15:06:33 +0100

    Ajout babel, webpack...etc

---
+++ src/Calendar.js
@@ -0,0 +1,16 @@
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class Calendar extends Component {
+  render() {
+    return (
+      <div className="Calendar">
+      <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
